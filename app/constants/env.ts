3ed08ec0afe95dea5dcac3680797ccbcfaf65237
adamdelezuch89 <adamdelezuch89@gmail.com>
2025-03-23 18:27:29 +0100

    some changes

--- app/constants/env.ts
+++ app/constants/env.ts
@@ -1,8 +1,16 @@
 import Constants from 'expo-constants';
 
-export const API_URL = Constants.expoConfig?.extra?.API_URL as string;
-export const DEBUG = Constants.expoConfig?.extra?.DEBUG as boolean;
+// Fallback wartości dla zmiennych środowiskowych
+const DEFAULT_API_URL = 'https://smartcook.addev.pl/';
+const DEFAULT_DEBUG = false;
 
+// Próba pobrania z Constants, jeśli nie jest dostępna, użyj wartości domyślnej
+export const API_URL = Constants.expoConfig?.extra?.API_URL || DEFAULT_API_URL;
+export const DEBUG = Constants.expoConfig?.extra?.DEBUG !== undefined 
+  ? Constants.expoConfig?.extra?.DEBUG as boolean 
+  : DEFAULT_DEBUG;
+
+// Sprawdzenie czy API_URL jest zdefiniowane - już nie rzucamy błędu, tylko logujemy ostrzeżenie
 if (!API_URL) {
-  throw new Error('API_URL is not defined in environment variables');
+  console.warn('API_URL is not defined in environment variables, using default value');
 } 
